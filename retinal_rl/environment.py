2141c40e6aede4a29c03a35ca583a403faef3e20
alex404 <alex404@mailbox.org>
2022-01-20 17:06:02 +0100

    Added reward_scale variable

--- retinal_rl/environment.py
+++ retinal_rl/environment.py
@@ -523,7 +523,7 @@ class RetinalSpec:
         # expect list of tuples (wrapper_cls, wrapper_kwargs)
         self.extra_wrappers = extra_wrappers
 
-def generate_retinal_spec(nm0):
+def generate_retinal_spec(nm0,scl):
 
     # absolute path needs to be specified, otherwise Doom will look in the SampleFactory scenarios folder
     nm = nm0[8:]
@@ -532,7 +532,7 @@ def generate_retinal_spec(nm0):
             'retinal_' + nm,
             join(os.path.abspath('scenarios'), nm + '.cfg'),  # use your custom cfg here
             doom_action_space_basic(),
-            reward_scaling=1)
+            reward_scaling=scl)
 
     return spec
 
@@ -540,7 +540,7 @@ def generate_retinal_spec(nm0):
 # noinspection PyUnusedLocal
 def make_retinal_env(nm, cfg,  **kwargs):
 
-    spec = generate_retinal_spec(nm)
+    spec = generate_retinal_spec(nm,cfg.reward_scale)
 
     episode_horizon=None
     fps = cfg.fps if 'fps' in cfg else None
