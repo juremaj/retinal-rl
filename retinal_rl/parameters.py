403c9a6934bd0283483c1d467dae0431668c8625
juremaj <majnik.jure@gmail.com>
2022-02-18 14:21:26 +0100

    added stride parameter for lindsay conv1/2

--- retinal_rl/parameters.py
+++ retinal_rl/parameters.py
@@ -43,7 +43,8 @@ def custom_parse_args(argv=None, evaluation=False):
     parser.add_argument('--retinal_bottleneck', type=int, default=4, help='Number of channels in retinal bottleneck')
     parser.add_argument('--vvs_depth', type=int, default=1, help='Number of CNN layers in the ventral stream network')
     parser.add_argument('--kernel_size', type=int, default=7, help='Size of CNN filters')
-    parser.add_argument('--rf_ratio', type=int, default=3, help='Ratio between RFs of first (BC) and second (RGC) convolutional layer in Mosaic network')
+    parser.add_argument('--retinal_stride', type=int, default=1, help='Stride at first two conv layers (\'BC\' and \'RGC\'), doesnt apply to \'VVS\'')
+    parser.add_argument('--rf_ratio', type=int, default=3, help='Ratio between RFs of first (\'BC\') and second (\'RGC\') convolutional layer in Mosaic network')
     parser.add_argument('--analyze_acts', type=str, default='False', help='Visualize activations via gifs and dimensionality reduction; options: \'environment\', \'mnist\' or \'cifar\'') # specific for analyze.py
     parser.add_argument('--analyze_max_num_frames', type=int, default=1e3, help='Used for visualising \'environment\' activations (leave as defult otherwise), normally 1e5 works for a nice embedding') # specific for analyze.py
     parser.add_argument('--shape_reward', type=bool, default=True, help='Turns on reward shaping')
