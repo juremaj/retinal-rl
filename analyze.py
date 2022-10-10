de74f5a1cf8ec2454a5b6a706ebd188e6d8c3cbf
juremaj <majnik.jure@gmail.com>
2022-10-10 17:15:23 +0200

    refactored analyze script and accompanying library

--- analyze.py
+++ analyze.py
@@ -1,57 +1,29 @@
-# System
 import sys
 
-# Numerics
-import torch
-import numpy as np
+from retinal_rl.environment import register_retinal_environment
+from retinal_rl.encoders import register_encoders
+from retinal_rl.parameters import custom_parse_args
 
-# retinal-rl
-from retinal_rl.visualization import *
-from retinal_rl.activations import *
-
+from retinal_rl.activations import get_env_ac, simulate, load_sim_out, get_acts_dataset
+from retinal_rl.plot import save_simulation_gif, plot_all_rf, plot_acts_tsne_stim, plot_dimred_ds_acts
 
 def analyze(cfg):
+    env, actor_critic = get_env_ac(cfg)
+    simulate(cfg, env, actor_critic) # this saves
 
-    cfg = load_from_checkpoint(cfg)
-    max_num_frames = cfg.analyze_max_num_frames
-    device = torch.device('cpu' if cfg.device == 'cpu' else 'cuda') # this line should go above the get_acts_actor function
-    
-    # getting encoder, environment info and activations of layers during experience
-    enc, env_infos, all_acts_dict_enc = get_acts_environment(cfg, max_num_frames=max_num_frames) # all_acts_dict (keys correspond to layers)
+    # load simulated data
+    sim_out = load_sim_out(cfg)
 
-    # always make simulation gif
-    save_simulation_gif(cfg,all_acts_dict_enc['imgs'][:1000]) # only first 1000 frames due to .gif resource limitations
-    
-    # always plot receptive fields
-    n_conv_lay = len(enc.conv_head)//2
-    for lay in range(1, n_conv_lay+1):
-        save_receptive_fields_plot(cfg,device,enc,lay,all_acts_dict_enc['obs_torch'])
+    # visualise
+    plot_all_rf(cfg, actor_critic, env) # receptive fields
+    save_simulation_gif(cfg, sim_out['all_img'])
 
-    # optional analysis (activations in environment and to dataset images)
-    if cfg.analyze_acts == 'environment':        
-        # visualizing conv layers
-        for lay in range(n_conv_lay):
-            save_activations_gif(cfg, all_acts_dict_enc['imgs'][:1000], all_acts_dict_enc['conv_acts'][lay][:1000], lay) # only first 1000 frames due to .gif resource limitations
-        # visualizing PCA of fully connected layer
-        plot_PCA_env(cfg, all_acts_dict_enc['imgs'], env_infos, all_acts_dict_enc['fc_acts'], all_acts_dict_enc['v_acts'], n_pcs=50)
-        # visualizing tSNE of fully connected layer and rnn
-        plot_tsne_env(cfg, all_acts_dict_enc)
+    plot_acts_tsne_stim(cfg, sim_out['all_fc_act'], sim_out['all_health'], title='FC')
+    plot_acts_tsne_stim(cfg, sim_out['all_rnn_act'], sim_out['all_health'], title='RNN')
 
     if cfg.analyze_acts == 'dataset':
-        
-        if cfg.analyze_ds_name == 'CIFAR':
-            rewards_dict = {0:2, 1:1, 2:4, 3:5, 4:7, 5:6, 6:8, 7:9, 8:3, 9:0} # defined by the class-reward assignments in the .wad file
-        elif cfg.analyze_ds_name == 'MNIST':
-            rewards_dict = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9} # defined by the class-reward assignments in the .wad file (matching digits in case of mnist)
-
-        all_acts_dict_ds = get_acts_dataset(cfg, enc, cfg.analyze_ds_name, rewards_dict)
-        plot_dimred_ds(cfg, all_acts_dict_ds['fc_acts_np'], all_acts_dict_ds['all_lab_np'])
-        
-        # TODO: add function for classifier analysis
-    
-    elif cfg.analyze_acts != 'False':  # if anything other than 'False', 'environment', 'mnist' or 'cifar'
-        print('\n\nInvalid cfg.analyze_acts input, terminating script.\n\n')
-
+        ds_out = get_acts_dataset(cfg, actor_critic)
+        plot_dimred_ds_acts(cfg, ds_out['all_fc_act'], ds_out['all_lab'])
 
 def main():
     """Script entry point."""
--- retinal_rl/visualization.py
+++ retinal_rl/visualization.py
@@ -1,32 +1,38 @@
-# Science
-from functools import cmp_to_key
+from pygifsicle import optimize
+import imageio
+import numpy as np
 import torch
+from torch.utils import tensorboard
+
+
+from sample_factory.utils.utils import AttrDict
+from sample_factory.algorithms.appo.actor_worker import transform_dict_observations
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import imageio
-from pygifsicle import optimize
-from torch.utils.tensorboard import SummaryWriter
 
 from openTSNE import TSNE
 
-#from torchinfo import summary
-#from captum.attr import NeuronGradient
 
-
-def save_simulation_gif(cfg,imgs, lay=0):
+def save_simulation_gif(cfg, all_img):
     t_stamp =  str(np.datetime64('now')).replace('-','').replace('T','_').replace(':', '')
-    pth = cfg.train_dir + "/" + cfg.experiment + f"/sim_" + t_stamp + ".gif"
+    pth = f'{cfg.train_dir}/{cfg.experiment}/sim_{t_stamp}.gif'
 
     wrt = imageio.get_writer(pth, mode='I',fps=35)
 
     with wrt as writer:
-        for img in imgs:
-            writer.append_data(img)
+        for i in range(all_img.shape[3]):
+            writer.append_data(all_img[:,:,:,i])
 
     optimize(pth)
 
-def save_receptive_fields_plot(cfg,device,enc,lay,obs_torch):
+
+def save_receptive_fields_plot(cfg,device,enc,lay,env):
+    
+    obs = env.reset() # this is the first observation when agent is spawned
+    obs_torch = AttrDict(transform_dict_observations(obs))
+    for key, x in obs_torch.items():
+        obs_torch[key] = torch.from_numpy(x).to(device).float()
+    
     isz = list(obs_torch['obs'].size())[1:]
     outmtx = enc.conv_head[0:(lay*2)](obs_torch['obs'])
     osz = list(outmtx.size())[1:]
@@ -72,7 +78,7 @@ def save_receptive_fields_plot(cfg,device,enc,lay,obs_torch):
 
     # displaying in tensorboard
     pth_tb = cfg.train_dir +  "/" + cfg.experiment + '/.summary/0/'
-    writer = torch.utils.tensorboard.SummaryWriter(pth_tb)
+    writer = tensorboard.SummaryWriter(pth_tb)
     writer.add_figure(f"rf-conv{lay}", fig, close=False) # only show the latest rfs in tb (for comparison across models)
     writer.close()
 
@@ -80,6 +86,14 @@ def save_receptive_fields_plot(cfg,device,enc,lay,obs_torch):
     pth = cfg.train_dir +  "/" + cfg.experiment + f"/rf-conv{lay}_" + t_stamp + ".png"
     plt.savefig(pth)
 
+def plot_all_rf(cfg, actor_critic, env):
+    enc = actor_critic.encoder.base_encoder
+    device = torch.device('cpu' if cfg.device == 'cpu' else 'cuda') # this line should go above the get_acts_actor function
+
+    # always plot receptive fields
+    n_conv_lay = len(enc.conv_head)//2
+    for lay in range(1, n_conv_lay+1):
+        save_receptive_fields_plot(cfg,device,enc,lay,env)
 
 def spike_triggered_average(dev,enc,lay,flt,rds,isz):
 
@@ -101,202 +115,85 @@ def spike_triggered_average(dev,enc,lay,flt,rds,isz):
 
     return avg
 
-def save_activations_gif(cfg, imgs, conv_acts, lay):
+def fit_tsne_1d(data):
+    print('fitting 1d-tSNE...')
+    # default openTSNE params
+    tsne = TSNE(
+        n_components=1,
+        perplexity=30,
+        initialization="pca",
+        metric="euclidean",
+        n_jobs=8,
+        random_state=3,
+    )
 
-    snapshots = np.asarray(conv_acts)
-    fps = 30
-    nSeconds = round(len(snapshots)//fps)
+    tsne_emb = tsne.fit(data)
+    return tsne_emb
 
-    nflts = snapshots[0].shape[2]
-    rwsmlt = 2 if nflts > 8 else 1 # number of rows
-    fltsdv = nflts//rwsmlt + 1 # numer of clumns (+ 1 for rgb image)
-
-    fig, axs = plt.subplots(rwsmlt,fltsdv,dpi=1, figsize=(128*fltsdv, 72*rwsmlt))
-
-    ims = []
-    vmaxs = [abs(snapshots[:,:,:,i]).max() for i in range(nflts)]
-
-    print(f'Visualising activations for conv{lay+1}')
-
-    for t in range(fps*nSeconds-1):
-        pts = []
-
-        ax = axs[0,0] if rwsmlt>1 else axs[0]
-        im = ax.imshow(imgs[t][:,:,:], interpolation='none', aspect='auto', vmin=-0, vmax=256) # plotting pixels
-        ax.axis('off')
-        ax = axs[1,0] if rwsmlt>1 else axs[0]
-        ax.axis('off')
-        pts.append(im)
-
-        flt=0 # counter
-        for i in range(fltsdv-1):
-            for j in range(rwsmlt):
-
-                ax = axs[j, i+1] if rwsmlt>1 else axs[i+1]
-                im = ax.imshow(snapshots[t,:,:,flt], interpolation='none', aspect='auto', vmin=0, vmax=vmaxs[flt]) # plotting activations
-                flt +=1
-                ax.axis('off')
-                pts.append(im)
-
-        ims.append(pts)
-
-        if t % (fps*10) == 0:
-            print('Progress: ', t//fps, '/', nSeconds, ' seconds')
-
-    anim = animation.ArtistAnimation(fig, ims, interval=1000/fps)
-    print('Saving video... (this can take some time for >10s simulations)')
-
-    t_stamp =  str(np.datetime64('now')).replace('-','').replace('T','_').replace(':', '')
-    pth = cfg.train_dir + "/" + cfg.experiment + f"/act-conv{lay+1}_" + t_stamp + ".gif"
-    anim.save(pth, fps=fps)
-
-    print('Done!')
-
-
-def plot_PCA_env(cfg, imgs, env_infos, fc_acts, v_acts, n_pcs=64):
-    rm_env_steps = 500
-    print(f'Removing first {rm_env_steps} to avoid value artefact due to rnn')
-    fc_acts_np = np.concatenate(fc_acts[rm_env_steps:], axis=0) # converting from list to np
-    v_acts_np = np.concatenate(v_acts[rm_env_steps:])
-    env_infos = env_infos[rm_env_steps:]
-
-    # preprocessing/reformatting health, value and pixels
-    health = np.zeros(len(env_infos))
-    pix_vect = np.zeros((len(env_infos), imgs[0].flatten().shape[0]))
-
-    for i in range(len(health)):
-        health[i] = env_infos[i]['HEALTH']
-        pix_vect[i,:] = imgs[i].flatten()
-
-    # centering and normalising
-    health_cent = (health - np.mean(health, axis=0))/np.max(health)
-
-
-    # fc activation pca
-    pca = torch.pca_lowrank(torch.from_numpy(fc_acts_np), q=n_pcs)
-    var_exp = pca[1].numpy()/np.sum(pca[1].numpy())
-    ll_states = pca[0].numpy()[:,0:n_pcs]
-    t_stamps =np.arange(0, len(ll_states))
-
-    # pixel pca
-    pix_pca = torch.pca_lowrank(torch.from_numpy(pix_vect), q=n_pcs)
-    pix_var_exp = pix_pca[1].numpy()/np.sum(pix_pca[1].numpy())
-    pix_ll_states = pix_pca[0].numpy()[:,0:n_pcs]
-
-    health = health_cent*2*np.max(ll_states[:,0]) + np.mean(ll_states[:,0]) #rescale and center to PC1 over time
-
-    plt.subplots(2, 2, gridspec_kw={'width_ratios': [5, 1], 'height_ratios': [5, 1]}, figsize=(20,10))
-
-    # plot of first two PCs
-    plt.subplot(2, 2, 1)
-    plt.scatter(ll_states[:,0], ll_states[:,1], s=0.1, c=v_acts_np, cmap='jet')
-    plt.colorbar(label="Value")
-    plt.ylabel('PC2')
-    plt.xlabel('PC1')
-
-    #scree plot
-    plt.subplot(2, 2, 2)
-    plt.plot(var_exp, label='PCA on output')
-    plt.plot(pix_var_exp, label='PCA on pixels')
-    plt.ylim(ymin=0)
-    plt.ylabel('Proportion var. explained')
-    plt.xlabel('PC number')
-
-    # first pc as time series
-    plt.subplot(2, 2, (3,4))
-    plt.plot(t_stamps/1000, normalize_data(ll_states[:,0]), label='PC1 output')
-    plt.plot(t_stamps/1000, normalize_data(pix_ll_states[:,0]), label='PC1 pixels')
-    plt.plot(t_stamps/1000, normalize_data(health), label='health', c='grey')
-    plt.plot(t_stamps/1000, normalize_data(v_acts_np), label='value', c='red')
-    plt.xlabel('Time(kStamps)')
-    plt.ylabel('PC1')
-    plt.legend()
-
-    # saving
-    t_stamp =  str(np.datetime64('now')).replace('-','').replace('T','_').replace(':', '')
-    pth = cfg.train_dir +  "/" + cfg.experiment + "/fc_pca_" + t_stamp + ".png"
-    plt.savefig(pth)
-
-def normalize_data(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
-
-def plot_tsne_env(cfg, all_acts_dict):
-    nn_acts_plot = ['fc_acts', 'rnn_acts'] # hard-coded which activations to plot
-
-    _, axs = plt.subplots(1,len(nn_acts_plot),figsize=(40,10))
-
-    for (i, nn_acts) in enumerate(nn_acts_plot):
-        print('\n Running tSNE for: ' + nn_acts)
-        plot_lay_tsne(all_acts_dict[nn_acts], all_acts_dict['v_acts'], axs[i])
-        axs[i].set_title(nn_acts)
-
-    # saving
-    t_stamp =  str(np.datetime64('now')).replace('-','').replace('T','_').replace(':', '')
-    pth = cfg.train_dir +  "/" + cfg.experiment + "/tsne_env_" + t_stamp + ".png"
-    plt.savefig(pth)
-
-def plot_lay_tsne(nn_acts, v_acts, ax):
-    rm_env_steps = 500
-    print(f'Removing first {rm_env_steps} to avoid value artefact due to rnn')
-    nn_acts_np = np.concatenate(nn_acts[rm_env_steps:], axis=0)
-    v_acts_np = np.concatenate(v_acts[rm_env_steps:])
-
+def fit_tsne(data):
+    print('fitting tSNE...')
+    # default openTSNE params
     tsne = TSNE(
         perplexity=30,
         initialization="pca",
         metric="euclidean",
         n_jobs=8,
-        random_state=42,
-        verbose=True,
+        random_state=3,
     )
 
-    embedding = tsne.fit(nn_acts_np)
+    tsne_emb = tsne.fit(data.T)
+    return tsne_emb
 
-    #plt.figure(figsize=(10,10))
-    sct = ax.scatter(embedding[:,0], embedding[:,1], s=0.1, c=v_acts_np,cmap='jet')
-    cbar = plt.colorbar(sct, ax=ax)
-    cbar.set_label(label='Value',size=20)
-    cbar.ax.tick_params(labelsize=50)
-    cbar.set_ticks([])
-    cbar.outline.set_visible(False)
-    ax.set_axis_off()
-
-def plot_dimred_ds(cfg, nn_acts_np, all_lab_np):
-
-    # computing PCA
-    n_pcs=64
-    pca = torch.pca_lowrank(torch.from_numpy(nn_acts_np), q=n_pcs)
-    ll_states = pca[0].numpy()[:,0:n_pcs]
-
-    # computing tSNE
-    tsne = TSNE(
-        perplexity=30,
-        initialization="pca",
-        metric="euclidean",
-        n_jobs=8,
-        random_state=42,
-        verbose=True,
-    )
-
-    embedding = tsne.fit(nn_acts_np)
-
-    # plotting both
-    fig, axs = plt.subplots(1, 2, figsize=(20,10))
-    sct1 = axs[0].scatter(ll_states[:,0], ll_states[:,1], s=0.6, c=all_lab_np,  cmap='jet')
-    axs[0].set_title('PCA')
-    cbar = plt.colorbar(sct1, ax=axs[0])
-    cbar.set_label(label='Stim id',size=20)
-    cbar.outline.set_visible(False)
-    axs[0].set_axis_off()
-
-    sct2 = axs[1].scatter(embedding[:,0], embedding[:,1], s=0.1, c=all_lab_np, cmap='jet')#, c=v_acts_np,cmap='jet') # AS A SANITY CHECK ALSO COLOR CODE BY TIME (SHOULD MATCH PROBABLY)
-    cbar = plt.colorbar(sct2, ax=axs[1])
-    axs[1].set_title('tSNE')
-    cbar.set_label(label='Stim id',size=20)
-    cbar.outline.set_visible(False)
-    axs[1].set_axis_off()
-
+def plot_acts_tsne_stim(cfg, acts, health, title):
+       
+    # zscore
+    data=row_zscore(acts)
+    # get tSNE sorting and resort data
+    embedding = fit_tsne_1d(data)
+    temp = np.argsort(embedding[:,0])
+    data = data[temp,:]
+    
+    # get stimulus collection times
+    pos_col = np.where(np.sign(get_stim_coll(health)) == 1)
+    neg_col = np.where(np.sign(get_stim_coll(health)) == -1)
+    
+    # plot
+    plt.figure(figsize=(10,3), dpi = 400)
+    plt.imshow(data, cmap='bwr', interpolation='nearest', aspect='auto', vmin=-4, vmax=4)
+    plt.vlines(pos_col, 0, data.shape[0], color='grey', linewidth=0.3, linestyle='--')
+    plt.vlines(neg_col, 0, data.shape[0], color='black', linewidth=0.3, linestyle=':')
+    plt.xlabel('Time (stamps)')
+    plt.ylabel(f'{title} unit id.')
+    plt.title(f'Activations of {title} neurons')
+    
     # saving
     t_stamp =  str(np.datetime64('now')).replace('-','').replace('T','_').replace(':', '')
-    pth = cfg.train_dir +  "/" + cfg.experiment + "/tsne_ds_" + t_stamp + ".png"
+    pth = cfg.train_dir +  "/" + cfg.experiment + f"/acts_tsne_{title}_sim_" + t_stamp + ".png"
     plt.savefig(pth)
+
+def get_stim_coll(all_health, health_dep=-8, death_dep=30):
+    
+    stim_coll = np.diff(all_health)
+    stim_coll[stim_coll == health_dep] = 0 # excluding 'hunger' decrease
+    stim_coll[stim_coll > death_dep] = 0 # excluding decrease due to death
+    stim_coll[stim_coll < -death_dep] = 0
+    return stim_coll
+
+# to plot library
+def row_zscore(mat):
+    return (mat - np.mean(mat,1)[:,np.newaxis])/(np.std(mat,1)[:,np.newaxis]+1e-8)
+
+def plot_dimred(cfg, embedding, c, title='embedding'):
+    fig = plt.figure(figsize=(20,20))
+    plt.scatter(embedding[:,0],embedding[:,1], s=5, c=c, cmap='jet')
+    plt.colorbar()
+    plt.axis('off')
+    
+    # saving
+    t_stamp =  str(np.datetime64('now')).replace('-','').replace('T','_').replace(':', '')
+    pth = cfg.train_dir +  "/" + cfg.experiment + f"/acts_tsne_FC_ds_" + t_stamp + ".png"
+    plt.savefig(pth)
+
+def plot_dimred_ds_acts(cfg, all_fc_act, all_lab):
+    tsne_emb = fit_tsne(all_fc_act)
+    plot_dimred(cfg, tsne_emb, all_lab)
