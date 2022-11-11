1ba7096b5ffec76ff9fc5acf7060cade9a226b1b
alex404 <alex404@mailbox.org>
2022-11-11 12:49:15 +0100

    Tiny clean up

--- retinal_rl/parameters.py
+++ retinal_rl/parameters.py
@@ -13,6 +13,8 @@ def add_retinal_env_args(env, parser):
     p.add_argument('--timelimit', default=None, type=float, help='Allows to override default match timelimit in minutes')
     p.add_argument('--res_w', default=128, type=int, help='Game frame width after resize')
     p.add_argument('--res_h', default=72, type=int, help='Game frame height after resize')
+    #p.add_argument('--frame_w', default=160, type=int, help='Game frame width')
+    #p.add_argument('--frame_h', default=120, type=int, help='Game frame height')
     p.add_argument('--wide_aspect_ratio', default=False, type=str2bool, help='If true render wide aspect ratio (slower but gives better FOV to the agent)')
 
 def retinal_override_defaults(env, parser):
@@ -48,17 +50,16 @@ def custom_parse_args(argv=None, evaluation=False):
     parser.add_argument('--vvs_depth', type=int, default=1, help='Number of CNN layers in the ventral stream network')
     parser.add_argument('--kernel_size', type=int, default=7, help='Size of CNN filters')
     parser.add_argument('--retinal_stride', type=int, default=2, help='Stride at the first conv layer (\'BC\'), doesnt apply to \'VVS\'')
-    parser.add_argument('--rf_ratio', type=int, default=3, help='Ratio between RFs of first (\'BC\') and second (\'RGC\') convolutional layer in Mosaic network')
     parser.add_argument( "--activation", default="elu" , choices=["elu", "relu", "tanh", "linear"]
                         , type=str, help="Type of activation function to use.")
     parser.add_argument("--greyscale", default=False, type=str2bool
                         , help="Whether to greyscale the input image.")
+    parser.add_argument('--shape_reward', type=str2bool, default=True, help='Turns on reward shaping')
 
     # for analyze script
     parser.add_argument('--analyze_acts', type=str, default='False', help='Visualize activations via gifs and dimensionality reduction; options: \'environment\', \'mnist\' or \'cifar\'') # specific for analyze.py
     parser.add_argument('--analyze_max_num_frames', type=int, default=1e3, help='Used for visualising \'environment\' activations (leave as defult otherwise), normally 100000 works for a nice embedding, but can take time') # specific for analyze.py
     parser.add_argument('--analyze_ds_name', type=str, default='CIFAR', help='Used for visualizing responses to dataset (can be \'MNIST\' or \'CIFAR\'') # specific for analyze.py
-    parser.add_argument('--shape_reward', type=str2bool, default=True, help='Turns on reward shaping')
 
     # SampleFactory parse_args function does some additional processing (see comments there)
     cfg = parse_args(argv=argv, evaluation=evaluation, parser=parser)
